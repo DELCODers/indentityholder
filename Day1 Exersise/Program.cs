db083f85238127b006ddf51fab7886361670aa3d
delmar <delmarpre@gmail.com>
2021-09-27 13:36:34 -0400

    First commit for day 1 exersise

---
+++ Day1 Exersise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day1_Exersise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to the identity taker");
+
+            Console.WriteLine("Can you give me your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Can you give me your age");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your name of your hometown");
+            string homeTown = Console.ReadLine();
+
+            //Console.WriteLine("Your name is"")
+            Console.WriteLine($"Your name is {name}. Your age is {age} Your hometown is {homeTown}");
+        }
+    }
+}
